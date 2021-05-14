26a538b1a698fcbfe0605807728eb7db00fa43b1
Belén <54420267+Yesica15@users.noreply.github.com>
2021-05-14 10:01:54 -0300

    7

--- login.js
+++ login.js
@@ -13,6 +13,6 @@ function validacion() {
       alert('[ERROR] La contraseña es demasiado corta. Ingrese al menos 4 caracteres');
       return false;
     }
-    window.location.href = "index2.html";
+    location.href = "index2.html";
     return true;
 }
